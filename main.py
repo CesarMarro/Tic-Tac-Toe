925f7d870ee46452a7f908ac1b60d061909ce6f5
CesarMarro <cemarroquin@ufm.edu>
2022-04-27 22:53:46 -0600

    check for winner 1.0
    First version of check for winner

--- main.py
+++ main.py
@@ -24,21 +24,32 @@ def update_board(board, positionx,positiony, player):
     board[positiony][positionx] = str(player)
 
 
-
-
-def check_for_winner(board, player):
+def check_for_winner(board):
     # TODO: evaluar si el jugador indicado ha ganado la partida.
-    pass
+    for i in range(len(board)):
+        if board[i][0] == board[i][1] and board[i][2]:
+            print("El ganador es: ", board[i][0])
+            return True
 
-
+        elif board[0][i] == board[1][i] and board[2][i]:
+            print("El ganador es: ", board[0][i])
+            return True
+            pass
+    return False
 '''
 Testing: 
 '''
-
+# winner = False
 board = create_empty_board()
 print_board(board)
-positionx = int(input("coordenada X: "))
-positiony = int(input("coordenada y: "))
-player = str(input("Jugador: "))
-update_board(board,positionx,positiony,player)
-print_board(board)
+
+while winner == False:
+
+    positionx = int(input("coordenada X: "))
+    positiony = int(input("coordenada y: "))
+    player = str(input("Jugador: "))
+    update_board(board,positionx,positiony,player)
+    print_board(board)
+    check_for_winner(board)
+    winner = check_for_winner(board)
+  
